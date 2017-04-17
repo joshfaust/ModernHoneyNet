09f62997fff9a11ca00893733ebf5d1f7ab1e8d5
MNFaust <faust.joshua@gmail.com>
2017-04-17 12:26:05 -0500

    Added root checker in MHN_Status

--- MHN_Status.sh
+++ MHN_Status.sh
@@ -5,11 +5,19 @@
 # By: Joshua Faust
 #======================================
 
+if [ $(whoami) != "root" ]
+	then
+	echo ""
+	echo "You must be root to check the supervosrctl Status."
+	echo "Checking Nginx status and Supervisor Status"
+fi
+
 echo '---------------------------------------------'
 echo '            Checking MHN status              '
 echo '---------------------------------------------'
+
 /etc/init.d/nginx status
 /etc/init.d/supervisor status
-sudo supervisorctl status
+supervisorctl status
 
 
